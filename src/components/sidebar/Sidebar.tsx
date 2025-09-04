b4c68f7c65f90848185a429204682f043e7e52d4
LittleT0fu <108378134+LittleT0fu@users.noreply.github.com>
2025-09-04 14:27:29 +0700

    Styling sidebar and add login button
    Refactors the Sidebar component for improved layout and styling, making it responsive for both desktop and mobile.
    
    Adds a logout button with an icon to the sidebar, and updates
    
    SidebarItem to support icon sizing and better hover effects.

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,17 +2,18 @@
 import React from "react";
 import { useUserRole, UserRole } from "@/context/userRoleContext";
 import { getNavItems, NavItem } from "@/config/navConfig";
+import { LogOut } from "lucide-react";
 
 function Sidebar() {
     const { role, setRole } = useUserRole();
     const navItems = getNavItems(role);
 
     return (
-        <div>
+        <div className="lg:flex lg:flex-col lg:items-start lg:w-[200px] lg:h-screen flex items-center flex-row gap-4 p-2">
             <div>
-                <h1>{role}</h1>
+                <h1 className="text-2xl font-bold capitalize px-4">{role}</h1>
             </div>
-            <div>
+            <div className="lg:flex lg:flex-col lg:h-full w-full flex flex-row gap-4">
                 {navItems.map((item) => (
                     <SidebarItem
                         key={item.label}
@@ -20,6 +21,17 @@ function Sidebar() {
                         setRole={setRole}
                     />
                 ))}
+            </div>
+            <div className="">
+                <SidebarItem
+                    key={"logout"}
+                    item={{
+                        label: "Logout",
+                        icon: LogOut,
+                        actions: { type: "logout", role: role },
+                    }}
+                    setRole={setRole}
+                />
             </div>
         </div>
     );
@@ -36,14 +48,14 @@ const SidebarItem = ({
 }) => {
     return (
         <div
-            className="cursor-pointer flex items-center gap-2"
+            className="cursor-pointer lg:w-full flex p-2 gap-2 text-base hover:bg-[#EAF5F9] rounded-lg"
             onClick={() => {
                 if (item.actions?.type === "switchRole") {
                     setRole(item.actions.role);
                 }
             }}
         >
-            {item.icon && <item.icon />}
+            {item.icon && <item.icon size={24} />}
             <span>{item.label}</span>
         </div>
     );
