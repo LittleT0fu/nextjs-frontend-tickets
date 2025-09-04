3b4416a53d39f881eaed5ceef75276f73520bba1
LittleT0fu <108378134+LittleT0fu@users.noreply.github.com>
2025-09-04 14:38:24 +0700

    Refactor Sidebar layout and adjust item styles
    Updated Sidebar component to improve layout structure for better responsiveness and maintainability.
    
    Fix mess for smaller screen size for better responsiveness

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,29 +9,31 @@ function Sidebar() {
     const navItems = getNavItems(role);
 
     return (
-        <div className="lg:flex lg:flex-col lg:items-start lg:w-[200px] lg:h-screen flex items-center flex-row gap-4 p-2">
+        <div className="lg:flex lg:flex-col items-start lg:w-[200px] lg:h-screen flex flex-col gap-4 p-2">
             <div>
                 <h1 className="text-2xl font-bold capitalize px-4">{role}</h1>
             </div>
-            <div className="lg:flex lg:flex-col lg:h-full w-full flex flex-row gap-4">
-                {navItems.map((item) => (
+            <div className="sm:flex lg:flex-col lg:h-full w-full">
+                <div className="lg:flex lg:flex-col lg:h-full w-full flex flex-row gap-4">
+                    {navItems.map((item) => (
+                        <SidebarItem
+                            key={item.label}
+                            item={item}
+                            setRole={setRole}
+                        />
+                    ))}
+                </div>
+                <div className="">
                     <SidebarItem
-                        key={item.label}
-                        item={item}
+                        key={"logout"}
+                        item={{
+                            label: "Logout",
+                            icon: LogOut,
+                            actions: { type: "logout", role: role },
+                        }}
                         setRole={setRole}
                     />
-                ))}
-            </div>
-            <div className="">
-                <SidebarItem
-                    key={"logout"}
-                    item={{
-                        label: "Logout",
-                        icon: LogOut,
-                        actions: { type: "logout", role: role },
-                    }}
-                    setRole={setRole}
-                />
+                </div>
             </div>
         </div>
     );
@@ -48,14 +50,14 @@ const SidebarItem = ({
 }) => {
     return (
         <div
-            className="cursor-pointer lg:w-full flex p-2 gap-2 text-base hover:bg-[#EAF5F9] rounded-lg"
+            className="cursor-pointer lg:w-full flex p-2 gap-2 sm:text-sm text-xs hover:bg-[#EAF5F9] rounded-lg"
             onClick={() => {
                 if (item.actions?.type === "switchRole") {
                     setRole(item.actions.role);
                 }
             }}
         >
-            {item.icon && <item.icon size={24} />}
+            {item.icon && <item.icon size={16} />}
             <span>{item.label}</span>
         </div>
     );
